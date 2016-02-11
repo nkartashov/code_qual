2343bdb226921f22435e72527245946d1c1c5dcf
kate <katejim@mail.ru>
2016-02-11 21:54:37 +0300

    add drone

---
+++ src/hashcode/solution/Drone.java
@@ -0,0 +1,35 @@
+package hashcode.solution;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kate on
+ * 11.02.16.
+ */
+public class Drone {
+    private final Located located;
+    private final Map<Integer, Integer> items = new HashMap<>(); // type, count
+
+    public Drone(Located located) {
+        this.located = located;
+    }
+
+    private int stepsFromBegin;
+
+    public Located getLocated() {
+        return located;
+    }
+
+    public Map<Integer, Integer> getItems() {
+        return items;
+    }
+
+    public int getStepsFromBegin() {
+        return stepsFromBegin;
+    }
+
+    public void setStepsFromBegin(int stepsFromBegin) {
+        this.stepsFromBegin = stepsFromBegin;
+    }
+}
