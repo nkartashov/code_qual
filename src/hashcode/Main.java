0f407acd21383198b47ca4fab8f988092dd345fb
kate <katejim@mail.ru>
2016-02-11 22:01:03 +0300

    write output file

--- src/hashcode/Main.java
+++ src/hashcode/Main.java
@@ -8,7 +8,7 @@ import hashcode.solution.SimpleSolution;
 public class Main {
 
     public static void main(String[] args) {
-	    for (String arg: args) {
+        for (String arg: args) {
             InputReader reader = new InputReader(arg);
             ISolution solution = new SimpleSolution(reader.readFile());
             ResultPrinter resultPrinter = new ResultPrinter(arg + ".out");
